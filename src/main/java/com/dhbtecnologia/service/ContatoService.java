e0af5c0c095fe29768045a2980fd886b22ff4e6b
romulolima <romulolima@tre-pa.jus.br>
2021-12-15 13:55:15 -0300

    findById, insert, update e delete

--- src/main/java/com/dhbtecnologia/service/ContatoService.java
+++ src/main/java/com/dhbtecnologia/service/ContatoService.java
@@ -2,11 +2,16 @@ package com.dhbtecnologia.service;
 
 import java.util.List;
 
+
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dhbtecnologia.domain.Contato;
+
 import com.dhbtecnologia.repository.ContatoRepository;
+
 
 @Service 
 public class ContatoService {
@@ -19,7 +24,23 @@ public class ContatoService {
 	}
 	
 	public Contato findById(Long id) {
+		return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("contato não encontrado"));
+	}
+	
+	public Contato insert(Contato contato) {
+		return repository.save(contato);
 		
+	}
+	
+	public Contato update(Long id, Contato contato) {
+		if(!repository.existsById(id)) throw new EntityNotFoundException("Tipo de contato não encontrado");
+		return repository.save(contato);
+		
+	}
+	
+	public void delete(Long id) {
+		Contato contato = this.findById(id);
+		repository.delete(contato);
 	}
 
 }
